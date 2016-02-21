f7f539a490c261561d39f80c4781f0dac49515fe
Stefan 'hr' Berder <hr@bonz.org>
2016-02-22 00:20:28 +0800

    put SECRET_KEY in a separate file & not in repo

--- ces_asia/config.py
+++ ces_asia/config.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 import os.path
+import sys
+
+try:
+    import secret
+except ImportError:
+    sys.stderr.write(""">> You should create secret.py next to config.py to define the secret key:
+# -*- coding: utf-8 -*-
+SECRET_KEY = 'qwerty123456'
+""")
+    raise
 
 LANGUAGES = ['fr', 'en', 'zh']
 BABEL_DEFAULT_LOCALE = 'fr'
-SECRET_KEY = 'test'
+SECRET_KEY = secret.SECRET_KEY
 
 # Bower configuration
 BOWER_PATH = os.path.join(os.path.dirname(__file__),
